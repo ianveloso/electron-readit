f1509f4f91d7050657750c30c45b9e6a24c6a9fd
Ian Veloso <dev@ianveloso.com>
2021-06-08 10:58:47 -0700

    chore: build config

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function createWindow () {
   state.manage(mainWindow)
 
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Listen for window being closed
   mainWindow.on('closed',  () => {
