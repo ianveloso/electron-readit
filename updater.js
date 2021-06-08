2edb1b67a938e8e7742118acee6b8749d0129ac0
Ian Veloso <dev@ianveloso.com>
2021-06-08 13:27:55 -0700

    feat: auto updater prompt

--- updater.js
+++ updater.js
@@ -1,11 +1,47 @@
+const { dialog } = require('electron');
 const { autoUpdater } = require('electron-updater');
 
 autoUpdater.logger = require('electron-log');
 autoUpdater.logger.transports.file.level = 'info';
 
+// Disable auto downloading of updates
+autoUpdater.autoDownload = false;
+
 module.exports = () => {
 
     // Check for update (GH Releases)
     autoUpdater.checkForUpdates();
-    console.log('Checking for updates');
+
+    // Listen for update-available
+    autoUpdater.on('update-available', () => {
+
+        // Prompt user to start download
+        dialog.showMessageBox({
+            type: 'info',
+            title: 'Update available',
+            message: 'A new version of Readit is available. Do you want to update now?',
+            buttons: [
+                'Update',
+                'No',
+            ]
+        }).then( result => {
+            let buttonIndex = result.response;
+
+            if( buttonIndex === 0) autoUpdater.downloadUpdate();
+        })
+    })
+
+    autoUpdater.on('update-downloaded', () => {
+        // Prompt user to install the update
+        dialog.showMessageBox({
+            type: 'info',
+            title: 'Update ready',
+            message: 'Install & restart now?',
+            buttons: ['Yes', 'Later']
+        }).then( result => {
+            let buttonIndex = result.response;
+
+            if( buttonIndex === 0) autoUpdater.quitAndInstall(false, true);
+        })
+    })
 }
